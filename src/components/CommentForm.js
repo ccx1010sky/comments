307014128fceae3596948684f3e8c835ec81eb02
ccx1010sky <ccx1010@gmail.com>
2022-01-10 16:01:23 +0000

    onChange

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
 
 const CommentForm = () =>{
+
+    const [author, setAuthor] = useState("");
+    const [text, setText] = useState("");
+   const handleAuthorChange = (event) =>{
+       setAuthor(event.target.value)
+
+   }
+   const handleTextChange = (event) =>{
+       setText(event.target.value);
+
+   }
+
 return(
     <form>
         <input 
             type="text" 
             placeholder="Your name"
+            value = {author}
+            onChange={handleAuthorChange}
 
         />
         <input 
             type="text"
             placeholder="Say something"
+            value = {text}
+            onChange={handleTextChange}
          />
         
         <input 
